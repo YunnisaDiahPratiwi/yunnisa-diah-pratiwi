2602d885668219aec43111f56d3b345fe1409bde
YunnisaDiahPratiwi <112565636+YunnisaDiahPratiwi@users.noreply.github.com>
2022-09-27 20:19:24 +0700

    Add files via upload

---
+++ TUMING2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namepspace std;
+int main(){
+	string nama, npm, kelas, jurusan;
+	getline(cin, nama);
+	getline(cin, npm);
+	getline(cin, kelas);
+	getline(cin, jurusan);
+
+	cout<<"nama: "<<nama<<endl;
+	cout<<"npm: "<<npm<<endl;
+	cout<<"kelas: "<<kelas<<endl;
+	cout<<"jurusan: "<<jurusan<<endl;
+	
+	return 0;
+}
