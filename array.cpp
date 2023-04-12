5a43b369ffdd5b4739d0aa8d4062f2f67ff40e00
YunnisaDiahPratiwi <112565636+YunnisaDiahPratiwi@users.noreply.github.com>
2023-04-12 20:44:05 +0700

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+//int main(){
+//	char name[20];
+//	
+//	cout<<"please enter your name:	";
+//	cin>>name;
+//	
+//	cout<<"hello, "<<name<<"!"<<endl;
+	//diarray ga bisa pakai getline
+	
+//	int myArray[5]={10, 20, 30, 40, 50};
+//	for(int i=0; i<5; i++){
+//		cout<<"value of element "<<i<<" is "<<myArray[i]<<endl;	
+	//}
+	
+//	int n;
+//	float rata, jumlah;
+//	cin>>n;
+//	int arr[n];
+//	for (int i=0; i<n; i++){
+//		cout<<"masukan angka:	"<<endl;
+//		cin>>arr[i];
+//		jumlah=jumlah + arr[i];
+//	}
+//
+//	cout<<"jumlahnya adalah:	"<<jumlah<<endl;
+//	rata=jumlah/n;
+//	cout<<"rata tara"<<rata;
+
+	//deklarasi array 2 dimensi
+//	int arr[3][4];
+//	
+//	for(int i=0; i<3; i++){
+//		for(int j=0; j<4; j++){
+//			arr[i][j]=i*j;
+//			cout<<arr[i][j]<<" ";
+//		}
+//		cout<<endl;
+//	}
+
+//	void printArray(int *arr, int size){
+//		for(int i=0; i<size; i++){
+//			cout<<arr[i]<<" ";
+//		}
+//		cout<<endl;
+//	}
+//	int main (){
+//		int arr[]={1, 2, 3, 4, 5};
+//		int size= sizeof(arr)/sizeof(int);
+//		cout<<sizeof(arr);
+//		cout<<sizeof(int); //sizeof untuk mengetahui ukuran atau isi array nya ada berapa
+//		
+//		
+//		//printArray(arr, size);
+//	}
+//	
